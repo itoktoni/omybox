a15ab46f67c0f742951449a43d4fba4399f70e83
itok toni laksono <itok.toni@gmail.com>
2020-07-08 00:44:39 +0700

    update

--- app/Dao/Models/Filters.php
+++ app/Dao/Models/Filters.php
@@ -7,16 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Filters extends Model
 {
 	protected $table = 'core_filters';
-	protected $primaryKey = 'key';
+	protected $primaryKey = 'filter';
 	protected $fillable = [
-		'key',
+		'filter',
 		'name',
 		'module',
 		'custom',
 		'field',
 		'function',
 		'operator',
-		'value',
+		'val',
 	];
 	public $incrementing = false;
 	public $timestamps = false;
