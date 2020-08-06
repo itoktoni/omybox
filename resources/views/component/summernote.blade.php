19cad89951f2bcfae266ffb9007db642cdb6c831
Itok toni laksono <itok.toni@gmail.com>
2020-08-07 02:07:04 +0700

    update

--- resources/views/component/summernote.blade.php
+++ resources/views/component/summernote.blade.php
@@ -1,4 +1,4 @@
-@if(!config('website.pjax'))
+
 @push('css')
 <link rel="stylesheet" href="{{ Helper::backend('vendor/summernote/summernote-lite.min.css') }}">
 @endpush
@@ -6,7 +6,7 @@
 @push('js')
 <script src="{{ Helper::backend('vendor/summernote/summernote-lite.min.js') }}"></script>
 @endpush
-@endif
+
 @if (isset($array) && in_array('lite', $array))
 @push('javascript')
 <script>
